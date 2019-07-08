b260ca8301b5bd86133155d82f2d81d5f172ea8a
Lorenzo Maturano <maturanolorenzo@gmail.com>
2019-07-08 11:32:41 -0300

    creating player class and displaying on screen

--- main.py
+++ main.py
@@ -6,7 +6,23 @@ pygame.init()
 pygame.display.set_caption('Raiden Fighters')
 gameScreen = pygame.display.set_mode((450,550))
 background = pygame.image.load('skins/bg.png')
+playerSkin = pygame.image.load('skins/player.png')
+playerSkin = pygame.transform.scale(playerSkin, (50,50))
+
+class Player:
+	def __init__(self,x,y,widht,height):
+		self.x = x
+		self.y = y
+		self.widht = widht
+		self.height = height
+
+	def draw(self,skin):
+		gameScreen.blit(skin, (self.x,self.y))
+
+player = Player(225,300,50,50)
 
 while True:
+	
 	gameScreen.blit(background,(0,0))
+	player.draw(playerSkin)
 	pygame.display.update()
