1435f59b7a29e931967ea29d39a334e8e8374308
Lorenzo Maturano <maturanolorenzo@gmail.com>
2019-07-11 08:09:23 -0300

    Screen class and scrolling screen method

--- main.py
+++ main.py
@@ -4,26 +4,49 @@ import pygame
 pygame.init()
 
 pygame.display.set_caption('Raiden Fighters')
-gameScreen = pygame.display.set_mode((450,550))
-background = pygame.image.load('skins/bg.png')
+# gameScreen = pygame.display.set_mode((gameScreenWidth,gameScreenHeight))
+# background = pygame.image.load('skins/bg.png')
 playerSkin = pygame.image.load('skins/player.png')
 playerSkin = pygame.transform.scale(playerSkin, (50,50))
 clock = pygame.time.Clock()
 
 class Player:
-	def __init__(self,x,y,widht,height):
+	def __init__(self,x,y,width,height):
 		self.x = x
 		self.y = y
-		self.widht = widht
+		self.width = width
 		self.height = height
 
 	def draw(self,skin):
-		gameScreen.blit(skin, (self.x,self.y))
+		screen.window.blit(skin, (self.x,self.y))
+
+class Screen:
+	def __init__(self,backgroundPath):
+		self.background = pygame.image.load(backgroundPath)
+		self.image_y = 0
+		self.gameScreenHeight = self.background.get_rect().height
+		self.gameScreenWidth = self.background.get_rect().width
+		self.window = pygame.display.set_mode((self.gameScreenWidth,self.gameScreenHeight))
+
+	def scrollScreen(self):
+		relative_y = self.image_y % self.gameScreenHeight
+		self.window.blit(self.background,(0,relative_y - self.gameScreenHeight))
+		if relative_y < self.gameScreenHeight:
+			self.window.blit(self.background, (0,relative_y))
+		self.image_y += 1
+
+# def scrollScreen():
+# 	relative_y =  % gameScreenHeight
+# 	gameScreen.blit(background,(0, relative_y - gameScreenHeight))
+# 	if relative_y < gameScreenHeight:
+# 		gameScreen.blit(background,(0,relative_y))
 
 player = Player(225,300,50,50)
+screen = Screen('skins/bg.png')
+
 
 while True:
 	clock.tick(27)
-	gameScreen.blit(background,(0,0))
+	screen.scrollScreen()
 	player.draw(playerSkin)
 	pygame.display.update()
